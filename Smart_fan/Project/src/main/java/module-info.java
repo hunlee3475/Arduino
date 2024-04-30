a809d32b51c8e381fc769b17a88317c78dd290ee
hunlee3475 <159232256+hunlee3475@users.noreply.github.com>
2024-04-30 15:15:54 +0900

    Add files via upload

---
+++ Smart_fan/Project/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.project {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.project to javafx.fxml;
+    exports com.example.project;
+
+    opens com.example.project.tcpip to javafx.fxml;
+    exports com.example.project.tcpip;
+}
